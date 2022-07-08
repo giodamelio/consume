23840ed273043a8609d2adbb21f8ce7fb4d03514
Gio d'Amelio <giodamelio@gmail.com>
2022-07-08 19:13:12 +0000

    Configure Phoenix to use Tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,9 @@ config :consume, ConsumeWeb.Endpoint,
   secret_key_base: "604w4gbtpQGFaY7VbTKSH/daImORSTDub7IFjfHTjSXXGJMmXg07evJ7+uijgqVn",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    # Setup tailwind dev watcher
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
