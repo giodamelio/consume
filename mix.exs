a67828b89c8b1a8db5ad332a7d11ad200dbb0ef9
Gio d'Amelio <giodamelio@gmail.com>
2022-07-08 01:13:29 +0000

    Add echo_psql_extras so we can see DB info in the LiveDashboard

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Consume.MixProject do
       {:phoenix, "~> 1.6.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ecto_psql_extras, "~> 0.7.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
