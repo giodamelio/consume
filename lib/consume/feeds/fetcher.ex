7ce869b86c39f7a2a756259aacfcc2f171c68721
Gio d'Amelio <giodamelio@gmail.com>
2022-07-11 00:47:29 +0000

    Add enable functionality to the fetcher GenServer. Add tests

--- lib/consume/feeds/fetcher.ex
+++ lib/consume/feeds/fetcher.ex
@@ -4,20 +4,48 @@ defmodule Consume.Feeds.Fetcher do
   ## Client API
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts ++ [name: __MODULE__])
+    initial_state = %{
+      enabled: false,
+      ticks_since_started: 0
+    }
+
+    GenServer.start_link(__MODULE__, initial_state, opts ++ [name: __MODULE__])
   end
 
-  def hello(name) do
-    GenServer.call(__MODULE__, {:hello, name})
+  def enable() do
+    GenServer.call(__MODULE__, {:set_enabled, true})
+  end
+
+  def disable() do
+    GenServer.call(__MODULE__, {:set_enabled, false})
   end
 
   ## GenServer callbacks
 
-  def handle_call({:hello, name}, _from, state) do
-    {:reply, "Hello #{name}!", state}
+  def handle_call({:set_enabled, true}, _from, state) do
+    timer()
+    {:reply, :enabled, %{state | enabled: true}}
+  end
+
+  def handle_call({:set_enabled, false}, _from, state) do
+    {:reply, :disabled, %{state | enabled: false}}
+  end
+
+  # Handles calling the fetcher once a second
+  def handle_info(:tick, state) do
+    if state.enabled do
+      timer()
+    end
+
+    {:noreply, %{state | ticks_since_started: state.ticks_since_started + 1}}
   end
 
   def init(args) do
     {:ok, args}
+  end
+
+  # Send a message to self() in 1 second
+  defp timer() do
+    Process.send_after(self(), :tick, 1_000)
   end
 end
--- test/consume/feeds/fetcher_test.exs
+++ test/consume/feeds/fetcher_test.exs
@@ -4,11 +4,20 @@ defmodule Consume.Feeds.FetcherTest do
   alias Consume.Feeds.Fetcher
 
   setup do
-    {:ok, _pid} = start_supervised(Fetcher)
-    :ok
+    {:ok, pid} = start_supervised(Fetcher)
+    %{pid: pid}
   end
 
-  test "says hello" do
-    assert Fetcher.hello("Gio") == "Hello Gio!"
+  test "doesn't tick if we don't enable it", %{pid: pid} do
+    Process.sleep(2500)
+    assert :sys.get_state(pid).ticks_since_started == 0
+  end
+
+  test "tick two times if we wait", %{pid: pid} do
+    :enabled = Fetcher.enable()
+    Process.sleep(2500)
+    :disabled = Fetcher.disable()
+
+    assert :sys.get_state(pid).ticks_since_started == 2
   end
 end
