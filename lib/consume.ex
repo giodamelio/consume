4061608f4cfdf5176d1871c377771aba28752e19
Gio d'Amelio <giodamelio@gmail.com>
2022-08-29 22:50:05 +0000

    mix phx.new

---
+++ lib/consume.ex
@@ -0,0 +1,9 @@
+defmodule Consume do
+  @moduledoc """
+  Consume keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+end
---
+++ lib/consume/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Consume.Mailer do
+  use Swoosh.Mailer, otp_app: :consume
+end
