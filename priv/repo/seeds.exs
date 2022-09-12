28a104796ddde33e14738cf6d67cd02ed0bd1628
Gio d'Amelio <giodamelio@gmail.com>
2022-09-12 22:21:23 +0000

    Create some Feeds in the seed script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,27 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Consume.Repo.insert!(%Consume.Fetcher.Feed{
+  name: "NASA Breaking News",
+  uri: "https://www.nasa.gov/rss/dyn/breaking_news.rss",
+  fetcher: :rss2_0,
+  fetch_after: DateTime.now!("Etc/UTC"),
+  fetch_interval_seconds: 10
+})
+
+Consume.Repo.insert!(%Consume.Fetcher.Feed{
+  name: "NASA Image of the Day",
+  uri: "https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss",
+  fetcher: :rss2_0,
+  fetch_after: DateTime.now!("Etc/UTC"),
+  fetch_interval_seconds: 10
+})
+
+Consume.Repo.insert!(%Consume.Fetcher.Feed{
+  name: "NASA Space Station News",
+  uri: "https://www.nasa.gov/rss/dyn/shuttle_station.rss",
+  fetcher: :rss2_0,
+  fetch_after: DateTime.now!("Etc/UTC"),
+  fetch_interval_seconds: 10
+})
